199c7111ff458d3776220c85597b2c11a5219c82
Rounak Handa <rounakhanda@Rounaks-MacBook-Pro.local>
2026-03-30 22:00:01 +0530

    fix image rendering FINAL FINAL

--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -6,9 +6,13 @@ const Item = (props) =>{
     return(
         <div className='item'>
             <Link to={`/product/${props.id}`}>
-                <img 
-  onClick={() => window.scrollTo(0,0)} 
-  src={typeof props.image === "object" ? props.image.default : props.image} 
+                <img
+  onClick={() => window.scrollTo(0, 0)}
+  src={
+    props.image?.default
+      ? props.image.default   // static import fix
+      : props.image           // backend/string
+  }
   alt=""
 />
             </Link>
